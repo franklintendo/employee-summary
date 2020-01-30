1d4e4e9a752a209815feb758bf021eac5e7517f6
Frank Moran <info@francisfrankmoran.com>
2020-01-30 16:04:48 -0500

    setting up employee classes

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const manager = require("./lib/manager.js");
+const engineer = require("./lib/engineer.js");
+const intern = require("./lib/intern.js");
 
 inquirer
 .prompt([
-    type = "input",
-    message = "What is your email?",
-    name = "email" 
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email" 
+    }, 
+    {
+        type: "input",
+        message: "What is your id?",
+        name: "id"
+    },
+    {
+        type: "list",
+        message: "What is your role?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern"]
+    }
 ])
-.then(console.log(data.email));
+.then(function(response){
+    console.log(response.email);
+});
+
